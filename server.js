d7bd1cfbdd39ffb43c2a14130c84532c45b1b5d4
Jonathan Huang <grokut@gmail.com>
2015-03-25 09:09:40 -0700

    began using passport-github auth

--- server.js
+++ server.js
@@ -9,22 +9,42 @@ var app = express();
 var mainExecution = require("./app");
 var util = require("./utilities");
 
+var passport = require('passport');
+var OAuth2Strategy = require('passport-oauth').OAuth2Strategy;
+var GitHubStrategy = require('passport-github').Strategy;
+
 var server = app.listen(3000, function(){
   var host = server.address().address;
   var port = server.address().port;
   console.log('example app listening at http://%s:%s', host, port);
 });
 
-app.use(express.static(__dirname + '/client/dist'));
-
 app.use(bodyParser.json());
 app.use(cors());
+
+app.use(session({
+  secret: 'nyan cat',
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/auth/github',
+  passport.authenticate('github'));
+
+app.get('/auth/github/callback',
+  passport.authenticate('github', { successRedirect: '/',
+                                      failureRedirect: '/login' }));
+
+// Serve Angular App
+app.use(express.static(__dirname + '/client/'));
 
 app.get('/numbers', function(req,res){
   var counts = util.getCounts();
   res.send(201, counts);
 });
-
 
 app.post('/', function(req,res) {
 });
