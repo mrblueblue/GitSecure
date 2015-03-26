e8c137d0fbf8d8adafe7cda5554d11e78df491b9
Camille Roussel <camille@rousselfamily.com>
2015-03-26 15:56:13 -0700

    Github Repo Registration

--- gitListener/gitHooks.js
+++ gitListener/gitHooks.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var request = require('request');
+var curl = require('curlrequest');
 
 // expects a git hub URL similar to 
 // https://github.com/:user/:repo with no trailing slash
@@ -9,23 +9,30 @@ var request = require('request');
 exports.registerRepo = function(repoUrl, authToken, unsubscribe) {
   var targetUrl = repoUrl + '/events/push.json';
   var subscribe = unsubscribe ? 'unsubscribe' : 'subscribe';
-  request.put({
+  console.log('registering repo');
+  curl.request({
     url: 'https://api.github.com/hub',
-    qs: {
+    method: 'POST',
+    headers: {
+      'User-Agent': 'graffiome/GitSecure',
+      'Authorization': 'token ' + authToken
+    },
+    data: {
       'hub.mode': subscribe,
       'hub.topic': targetUrl,
       'hub.callback': 'http://gitsecure.cloudapp.net:8080',
-    },
-    auth: {
-      username: '',
-      password: authToken
     }
   }, function(error, incoming, body){
+    console.log('registration response');
     if (error) {
       console.log('error:', error);
+    }
+    if (incoming) {
+      console.log('response', incoming);
     }
     if (body) {
       console.log('response', body);
     }
   });
+  console.log('registration sent');
 };
