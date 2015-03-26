03715235fb629ff3360f20d63d929ccb87d050c6
Rich Magee <rmagee88@gmail.com>
2015-03-26 14:20:15 -0700

    Refactored processRepo into module, moved app.js into server.js

--- server.js
+++ server.js
@@ -5,13 +5,17 @@ var bodyParser = require('body-parser');
 var cors = require('cors');
 var app = express();
 var util = require('./server/utilities.js');
-
-require('./server/app.js');
+var fileSystemUtilities = require('./server/services/fileSystem/utilities');
 
 var server = app.listen(3000, function(){
   var host = server.address().address;
   var port = server.address().port;
   console.log('example app listening at http://%s:%s', host, port);
+  // refactored from app.js, cleans out any un-deleted files that may exist in our git_data dir
+  console.log('Initializing GitSecure and cleaning from last cycle');
+  var pathToData = __dirname + '/git_data';
+  fileSystemUtilities.removeDirectorySync(pathToData);
+  console.log('system ready to process repos...');
 });
 
 app.use(express.static(__dirname + '/client/'));
