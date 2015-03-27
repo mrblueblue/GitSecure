d7fceac3c913642c210273cd1ef36170fcd182b3
Jonathan Huang <grokut@gmail.com>
2015-03-26 19:46:04 -0700

    added GET and POST routes for angular frontend

--- client/controllers.js
+++ client/controllers.js
@@ -4,7 +4,26 @@
 angular.module('main',[])
 .controller('mainController', function($scope, mainly, $http){
 
-  $scope.populateRepos = function() {
+  var collection = [
+    '28679810', 
+    '32131374',
+    '31937760'
+    ];
+
+  // Checkmarks a repo given a list of repos user has subscribed to
+  var checkRepo = function(collection){
+    var checkboxes = $('input:checkbox')
+    checkboxes.each(function(index, repo){
+      repo = $(repo)
+      var repo_id = $(repo).attr('data-repo-id');
+      console.log(repo, repo_id)
+      if ( collection.indexOf(repo_id) !== -1 ){
+        repo.prop('checked', true);
+      }
+    });
+  }
+
+  $scope.populateRepos = function(){
     console.log('populate called!');
     mainly.getRepos(function(data) {
       $scope.repos = data;
@@ -12,13 +31,28 @@ angular.module('main',[])
   };
 
   $scope.submit = function(e){
+    $http.get('/getRepos').success(function(data){
+        console.log(data);
+      })
     var checked = $(':checked');
-
     checked.each(function(index, repo){
-      var url = repo.getAttribute('id');
-      $http.post('some-url', url);
-    })
+      var data = {}
+      data.html_url = repo.getAttribute('data-url');
+      data.git_url = repo.getAttribute('data-git-url');
+      data.user_id = repo.getAttribute('data-user-id');
+      data.repo_id = repo.getAttribute('data-repo-id');
+      data.repo_name = repo.getAttribute('data-repo-name');
+      
+      // $http.post('/submitRepos', data).success(function(data){
+      //   console.log("this is the response: ", data);
+      // })
+
+      $http.get('/getRepos').success(function(data){
+        console.log(data);
+      })
+    });
   };
+
 })
 .factory('mainly', function($rootScope, $http){
   // function that gets repos for curr user
@@ -40,3 +74,25 @@ angular.module('main',[])
 
   return {getRepos: getRepos};
 });
+
+// Get all Repos users has subscribed
+
+// Add repo
+
+// Remove repo
+
+// Get reports for repos
+
+
+// repo url
+// git url
+// repo id
+
+
+var exdata = {
+  html_url: "https://github.com/mrblueblue/exercism", 
+  git_url: "git://github.com/mrblueblue/exercism.git", 
+  user_id: "9220038", 
+  repo_id: "28679810"
+}
+
--- server.js
+++ server.js
@@ -25,3 +25,36 @@ app.use(bodyParser.json());
 app.get('/numbers', function(req, res){
   res.send(201, {TBD:'0'});
 });
+
+// GET route for getting all subscribed repos
+
+app.get('/getRepos', function(req, res){
+
+  // response as an array of repo ids that
+  // the user has subscribed to
+
+  var collection = [
+    '28679810', 
+    '32131374',
+    '31937760'
+    ];
+
+  res.status(201).send(collection)
+
+  // console.log("This is the response: ", res)
+});
+
+// POST route for sending to DB all repos user wants to subscribe
+
+app.post('/submitRepos', function(req, res){
+
+  // req.body is an object (document)
+  // it can also be made into an object {data: repos}
+  // where repos is an array of objects (documents)
+
+  console.log("This is the request: ", req.body)
+
+  res.send({hello:'hello'})
+
+  // console.log("This is the response: ", res)
+});
