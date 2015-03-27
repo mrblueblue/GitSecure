1094354d55560aaad21df2bfc5be4e7c3f39a365
Jonathan Huang <grokut@gmail.com>
2015-03-27 15:24:20 -0700

    clean up

--- serverConfig-example.js
+++
@@ -1,7 +0,0 @@
-// DO NOT PUT REAL KEYS IN THIS FILE, IT IS FOR DEMONSTRATION PURPOSES ONLY!!!
-// CREATE A serverConfig.js LOCALLY WITH YOUR INFO
-
-// Githup ClientID, ClientSecret and CallbackURL
-exports.clientID = 'your client id';
-exports.clientSecret = 'your client secret';
-exports.callbackURL = 'your callbackURL';
