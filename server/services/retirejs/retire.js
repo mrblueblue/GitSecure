f7a9dffb26e8516a3caba624a099b166c6153f4e
Rich Magee <rmagee88@gmail.com>
2015-03-27 17:32:01 -0700

    Added logging to see dir for retirejs...

--- server/services/retirejs/retire.js
+++ server/services/retirejs/retire.js
@@ -3,11 +3,11 @@ var childProcess = require('child_process');
 
 // function to scan a directory using retirejs
 var retireScan = function(dir) {
+  console.log('RICH CHECK retireScan finished... dir to scan: ', dir);
   var results = childProcess.spawnSync('retire', [ '--jspath', dir, '--outputformat', 'json'], {encoding: 'utf8'});
 
   // returning the 2nd value in the output object
   // according to the docs this should be stderr (error value), but it's actually where our values are :D
-  console.log('RICH CHECK retireScan finished... resultsObj: ', results);
   return results ? results.output[2]: null;
 };
 
